7e5e0fcd5211be5b08e2f58d97b00fdf1fe6c765
Steve Steele <steven.harville.steele@gmail.com>
2022-09-26 10:34:09 -0500

    Standards

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Restaurant from './Restaurant'
 
 import '../../node_modules/foundation-sites/dist/css/foundation.min.css'
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 import '../css/App.css'
 
--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -3,4 +3,4 @@ const apiEndpoints = {
     tst: 'http://shs.restaurants.com:8888/api/?city=test',
     prd: 'https://restaurants.steve-steele.com/api/',
 }
-export const API_ENDPOINT = apiEndpoints.prd;
+export const API_ENDPOINT = apiEndpoints.prd
