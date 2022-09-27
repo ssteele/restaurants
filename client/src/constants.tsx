cd627afeab2915ad8f48d227a9da37a08b599af3
Steve Steele <steven.harville.steele@gmail.com>
2022-09-27 08:12:10 -0500

    Swap API endpoint based on react-scripts env

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -1,6 +1,11 @@
-const apiEndpoints = {
+const apiEndpoints: any = {
     loc: 'http://shs.restaurants.com:8888/api/',
     tst: 'http://shs.restaurants.com:8888/api/?city=test',
     prd: 'https://restaurants.steve-steele.com/api/',
 }
-export const API_ENDPOINT = apiEndpoints.prd
+
+let apiEnv: string = 'prd'
+if ('development' === process.env.NODE_ENV) {
+    apiEnv = 'loc'
+}
+export const API_ENDPOINT = apiEndpoints[apiEnv]
