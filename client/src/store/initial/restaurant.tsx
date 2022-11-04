db8baae4ed1e614b540333a2ff71eb72bba12cce
Steve Steele <steven.harville.steele@gmail.com>
2022-11-04 05:46:26 -0500

    Extract initial restaurant store
    ...and reuse

---
+++ client/src/store/initial/restaurant.tsx
@@ -0,0 +1,42 @@
+import {
+DEFAULT_MAX_NEARBY_ZIP_MI_DISTANCE,
+MAX_NEARBY_ZIP_MI_DISTANCE_OPTIONS,
+} from '../../constants'
+
+export const restaurant = {
+  categories: [],
+  current: null,
+  currentZipMeta: [],
+  error: {},
+  filteredCount: 0,
+  filteredIds: [],
+  geolocation: { isGeolocating: false },
+  isLoading: false,
+  modalIsOpen: false,
+  options: [
+    {name: 'nearby', description: 'Only near me', type: 'checkbox', value: false, rendered: true, disabled: true},
+    {
+      name: 'nearbyMaxMiles',
+      description: '... how close?',
+      type: 'select',
+      value: DEFAULT_MAX_NEARBY_ZIP_MI_DISTANCE,
+      values: MAX_NEARBY_ZIP_MI_DISTANCE_OPTIONS,
+      rendered: false,
+      disabled: true,
+    },
+    {name: 'kids', description: 'Only kid-friendly', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'organic', description: 'Only organic', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'local', description: 'Only locally sourced', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'vegetarian', description: 'Only vegetarian', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'keto', description: 'Only keto-friendly', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'meat', description: 'Only meat-friendly', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'indoor', description: 'Only indoor seating', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'outdoor', description: 'Only outdoor seating', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'playarea', description: 'Only with play area', type: 'checkbox', value: false, rendered: true, disabled: false},
+    {name: 'playground', description: 'Only with playground', type: 'checkbox', value: false, rendered: true, disabled: false},
+  ],
+  restaurantIds: [],
+  restaurants: null,
+  viewed: [],
+  viewIndex: null,
+}
--- client/src/store/reducers/restaurant.tsx
+++ client/src/store/reducers/restaurant.tsx
@@ -13,50 +13,11 @@ import {
   SET_MODAL,
   SET_ERROR,
 } from '../actions/restaurant'
-import {
-  DEFAULT_MAX_NEARBY_ZIP_MI_DISTANCE,
-  MAX_NEARBY_ZIP_MI_DISTANCE_OPTIONS,
-} from '../../constants'
+import { restaurant as initialRestaurantState } from '../initial/restaurant'
 import { IRestaurantStore } from '../../models/RestaurantStore'
 
 export const restaurant = (
-  state: IRestaurantStore = {
-    categories: [],
-    current: null,
-    currentZipMeta: [],
-    error: {},
-    filteredCount: 0,
-    filteredIds: [],
-    geolocation: { isGeolocating: false },
-    isLoading: false,
-    modalIsOpen: false,
-    options: [
-      {name: 'nearby', description: 'Only near me', type: 'checkbox', value: false, rendered: true, disabled: true},
-      {
-        name: 'nearbyMaxMiles',
-        description: '... how close?',
-        type: 'select',
-        value: DEFAULT_MAX_NEARBY_ZIP_MI_DISTANCE,
-        values: MAX_NEARBY_ZIP_MI_DISTANCE_OPTIONS,
-        rendered: false,
-        disabled: true,
-      },
-      {name: 'kids', description: 'Only kid-friendly', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'organic', description: 'Only organic', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'local', description: 'Only locally sourced', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'vegetarian', description: 'Only vegetarian', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'keto', description: 'Only keto-friendly', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'meat', description: 'Only meat-friendly', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'indoor', description: 'Only indoor seating', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'outdoor', description: 'Only outdoor seating', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'playarea', description: 'Only with play area', type: 'checkbox', value: false, rendered: true, disabled: false},
-      {name: 'playground', description: 'Only with playground', type: 'checkbox', value: false, rendered: true, disabled: false},
-    ],
-    restaurantIds: [],
-    restaurants: null,
-    viewed: [],
-    viewIndex: null,
-  },
+  state: IRestaurantStore = initialRestaurantState,
   action: any
 ): IRestaurantStore => {
   switch (action.type) {
