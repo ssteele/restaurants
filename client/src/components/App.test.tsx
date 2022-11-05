d03612cce01c987bb1e5ec99b7b94011e5d56839
Steve Steele <steven.harville.steele@gmail.com>
2022-11-05 14:38:47 -0500

    Add test

--- client/src/components/App.test.tsx
+++ client/src/components/App.test.tsx
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk'
 import { ConnectedApp } from '../index';
 import { restaurant } from '../store/initial/restaurant'
 import { GeolocationButton } from './GeolocationButton';
+import { OptionsButton } from './OptionsButton';
 import { OptionsModal } from './OptionsModal';
 
 const middlewares = [thunk]
@@ -29,18 +30,24 @@ describe('App component', () => {
     testInstance = testRenderer.root;
   })
 
-  it('connects to the restaurant store', () => {
+  it('properly renders the expected page', () => {
     expect(testRenderer.toJSON()).toMatchSnapshot()
   })
 
   it('properly loads the options modal', () => {
+    expect(() => testInstance.findByType(OptionsModal)).toBeTruthy()
     const optionsModal = testInstance.findByType(OptionsModal)
     expect(optionsModal.props.modalIsOpen).toBe(false)
     expect(optionsModal.props.filteredCount).toBe(0)
   })
 
   it('properly loads the geolocation button', () => {
+    expect(() => testInstance.findByType(GeolocationButton)).toBeTruthy()
     const geolocationButton = testInstance.findByType(GeolocationButton)
     expect(geolocationButton.props.geolocation.isGeolocating).toBe(false);
+  })
+
+  it('properly loads the options button', () => {
+    expect(() => testInstance.findByType(OptionsButton)).toBeTruthy()
   })
 })
