86efcef7111b939bd4e1e1172c4647402e9662fa
Steve Steele <steven.harville.steele@gmail.com>
2022-11-05 15:01:25 -0500

    Add test

--- client/src/components/App.test.tsx
+++ client/src/components/App.test.tsx
@@ -9,6 +9,7 @@ import { App } from './App';
 import { GeolocationButton } from './GeolocationButton';
 import { OptionsButton } from './OptionsButton';
 import { OptionsModal } from './OptionsModal';
+import { Restaurant } from './Restaurant';
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
@@ -32,24 +33,33 @@ describe('App component', () => {
   })
 
   it('properly renders the expected page', () => {
-    expect(() => testInstance.findByType(App)).toBeTruthy()
+    expect(() => testInstance.findByType(App)).not.toThrow(Error)
     expect(testRenderer.toJSON()).toMatchSnapshot()
   })
 
   it('properly loads the options modal', () => {
-    expect(() => testInstance.findByType(OptionsModal)).toBeTruthy()
+    expect(() => testInstance.findByType(OptionsModal)).not.toThrow(Error)
     const optionsModal = testInstance.findByType(OptionsModal)
     expect(optionsModal.props.modalIsOpen).toBe(false)
     expect(optionsModal.props.filteredCount).toBe(0)
   })
 
   it('properly loads the geolocation button', () => {
-    expect(() => testInstance.findByType(GeolocationButton)).toBeTruthy()
+    expect(() => testInstance.findByType(GeolocationButton)).not.toThrow(Error)
     const geolocationButton = testInstance.findByType(GeolocationButton)
     expect(geolocationButton.props.geolocation.isGeolocating).toBe(false);
   })
 
   it('properly loads the options button', () => {
-    expect(() => testInstance.findByType(OptionsButton)).toBeTruthy()
+    expect(() => testInstance.findByType(OptionsButton)).not.toThrow(Error)
+  })
+
+  it('properly loads the restaurant section', () => {
+    expect(() => testInstance.findByType(Restaurant)).not.toThrow(Error)
+    const restaurantSection = testInstance.findByType(Restaurant)
+    expect(restaurantSection.props.current).toBe(null);
+    expect(restaurantSection.props.error).toEqual({});
+    expect(restaurantSection.props.isLoading).toBe(false);
+    expect(restaurantSection.props.restaurants).toBe(null);
   })
 })
