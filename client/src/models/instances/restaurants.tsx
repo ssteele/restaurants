b24ee3b2db4f79e04bfee484922eab8692c57441
Steve Steele <steven.harville.steele@gmail.com>
2022-11-06 05:25:38 -0600

    Add test data

---
+++ client/src/models/instances/restaurants.tsx
@@ -0,0 +1,26 @@
+import { IRestaurant } from '../Restaurant';
+
+export const restaurantInstances: {[key: number]: IRestaurant} = {
+  1: {
+    id: 1,
+    name: 'Name 1',
+    sub_name: 'Sub Name 1',
+    categories: ['cat 1', 'cat 2'],
+    site: 'https://www.site-1.com',
+    menu: 'https://www.site-1.com/menu',
+    organic: true,
+    local: true,
+    vegetarian: true,
+    vegan: true,
+    keto: false,
+    indoor: true,
+    outdoor: true,
+    price: 1,
+    zips: [78753],
+    kids: ['Kids Meal'],
+    playground: true,
+    playarea: true,
+    enabled: true,
+    deleted: false,
+  },
+}
