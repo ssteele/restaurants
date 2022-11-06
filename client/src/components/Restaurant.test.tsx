1f666dcb8500f6357dd8cef5415b329e606d41cb
Steve Steele <steven.harville.steele@gmail.com>
2022-11-06 10:06:32 -0600

    Add tests for restaurants loading

--- client/src/components/Restaurant.test.tsx
+++ client/src/components/Restaurant.test.tsx
@@ -162,3 +162,56 @@ describe('Restaurant component with no restaurant selected', () => {
     expect(() => testInstance.findByProps({'data-id': 'restaurant-error-message'})).toThrow(Error)
   })
 })
+
+describe('Restaurant component while loading', () => {
+  let testRenderer: any
+  let testInstance: any
+  const current: number = 0
+  const error: object = {}
+  const isLoading: boolean = true
+  const restaurants: {[id: number]: IRestaurant} = {}
+
+  beforeEach(() => {
+    testRenderer = TestRenderer.create(
+      <Restaurant
+        current={current}
+        error={error}
+        isLoading={isLoading}
+        restaurants={restaurants}
+      ></Restaurant>
+    )
+    testInstance = testRenderer.root;
+  })
+
+  it('properly renders restaurant component', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByType(Restaurant)).not.toThrow(Error)
+  })
+
+  it('properly renders loading message', () => {
+    expect.assertions(2)
+    expect(() => testInstance.findByProps({'data-id': 'no-restaurants'})).not.toThrow(Error)
+    const noRestaurants = testInstance.findByProps({'data-id': 'no-restaurants'})
+    expect(noRestaurants.props.children).toBe('Loading...')
+  })
+
+  it('does not render restaurant link', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByProps({'data-id': 'restaurant-link'})).toThrow(Error)
+  })
+
+  it('does not render restaurant sub name', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByProps({'data-id': 'restaurant-sub-name'})).toThrow(Error)
+  })
+
+  it('does not render kids items', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByProps({'data-id': 'restaurant-kids-text'})).toThrow(Error)
+  })
+
+  it('does not show error message', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByProps({'data-id': 'restaurant-error-message'})).toThrow(Error)
+  })
+})
