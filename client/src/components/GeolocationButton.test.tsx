975ef8179e4e0b56367b41c644fc032aba3b3e11
Steve Steele <steven.harville.steele@gmail.com>
2022-11-06 10:20:41 -0600

    Add test

---
+++ client/src/components/GeolocationButton.test.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer';
+import * as thunks from '../store/thunks/restaurant'
+import { GeolocationButton } from './GeolocationButton';
+
+describe('Geolocation component', () => {
+  let testRenderer: any
+  let testInstance: any
+  const dispatch: any = jest.fn()
+  const geolocation = {isGeolocating: false}
+  // const prevRestaurantSpy = jest.spyOn(thunks, 'prevRestaurant')
+  // const nextRestaurantSpy = jest.spyOn(thunks, 'nextRestaurant')
+
+  beforeEach(() => {
+    testRenderer = TestRenderer.create(
+      <GeolocationButton
+        dispatch={dispatch}
+        geolocation={geolocation}
+      ></GeolocationButton>
+    )
+    testInstance = testRenderer.root;
+  })
+
+  it('properly renders geolocation button', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByType(GeolocationButton)).not.toThrow(Error)
+  })
+
+  // it('navigates to previous restaurant', () => {
+  //   expect.assertions(4)
+  //   const backButton = testInstance.findByProps({'data-id': 'nav-back-restaurant'})
+  //   expect(backButton.props.disabled).toBe(false);
+  //   backButton.props.onClick()
+  //   expect(dispatch).toHaveBeenCalledTimes(1)
+  //   expect(prevRestaurantSpy).toHaveBeenCalledTimes(1)
+  //   expect(nextRestaurantSpy).toHaveBeenCalledTimes(0)
+  // })
+
+  // it('navigates to next restaurant', () => {
+  //   expect.assertions(4)
+  //   const nextButton = testInstance.findByProps({'data-id': 'nav-next-restaurant'})
+  //   expect(nextButton.props.disabled).toBe(false);
+  //   nextButton.props.onClick()
+  //   expect(dispatch).toHaveBeenCalledTimes(1)
+  //   expect(prevRestaurantSpy).toHaveBeenCalledTimes(0)
+  //   expect(nextRestaurantSpy).toHaveBeenCalledTimes(1)
+  // })
+})
+
+// describe('Geolocation component with no restaurants', () => {
+//   let testRenderer: any
+//   let testInstance: any
+//   const dispatch: any = jest.fn()
+//   const filteredCount: number = 0
+
+//   beforeEach(() => {
+//     testRenderer = TestRenderer.create(
+//       <GeolocationButton
+//         dispatch={dispatch}
+//         filteredCount={filteredCount}
+//       ></GeolocationButton>
+//     )
+//     testInstance = testRenderer.root;
+//   })
+
+//   it('properly renders restaurant navigation', () => {
+//     expect.assertions(1)
+//     expect(() => testInstance.findByType(GeolocationButton)).not.toThrow(Error)
+//   })
+
+//   it('navigates to previous restaurant', () => {
+//     expect.assertions(1)
+//     const backButton = testInstance.findByProps({'data-id': 'nav-back-restaurant'})
+//     expect(backButton.props.disabled).toBe(true);
+//   })
+
+//   it('navigates to next restaurant', () => {
+//     expect.assertions(1)
+//     const nextButton = testInstance.findByProps({'data-id': 'nav-next-restaurant'})
+//     expect(nextButton.props.disabled).toBe(true);
+//   })
+// })
+
+// describe('Geolocation component with only 1 restaurant', () => {
+//   let testRenderer: any
+//   let testInstance: any
+//   const dispatch: any = jest.fn()
+//   const filteredCount: number = 1
+
+//   beforeEach(() => {
+//     testRenderer = TestRenderer.create(
+//       <GeolocationButton
+//         dispatch={dispatch}
+//         filteredCount={filteredCount}
+//       ></GeolocationButton>
+//     )
+//     testInstance = testRenderer.root;
+//   })
+
+//   it('properly renders restaurant navigation', () => {
+//     expect.assertions(1)
+//     expect(() => testInstance.findByType(GeolocationButton)).not.toThrow(Error)
+//   })
+
+//   it('navigates to previous restaurant', () => {
+//     expect.assertions(1)
+//     const backButton = testInstance.findByProps({'data-id': 'nav-back-restaurant'})
+//     expect(backButton.props.disabled).toBe(true);
+//   })
+
+//   it('navigates to next restaurant', () => {
+//     expect.assertions(1)
+//     const nextButton = testInstance.findByProps({'data-id': 'nav-next-restaurant'})
+//     expect(nextButton.props.disabled).toBe(true);
+//   })
+// })
