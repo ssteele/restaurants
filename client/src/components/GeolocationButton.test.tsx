6427da18111deaf5142c97d28bd1d62b74b8815b
Steve Steele <steven.harville.steele@gmail.com>
2022-11-08 15:22:56 -0600

    Add tests

--- client/src/components/GeolocationButton.test.tsx
+++ client/src/components/GeolocationButton.test.tsx
@@ -107,7 +107,7 @@ describe('Geolocation component with location during cooldown', () => {
   const geolocation: IGeolocation = {
     lat: 30.2642,
     lon: -97.7617,
-    timestamp: Date.now() - (GOOGLE_MAPS_COOL_OFF_SECONDS / 2),
+    timestamp: Date.now() - (1000 * GOOGLE_MAPS_COOL_OFF_SECONDS / 2),
     zip: 78704,
     isGeolocating: false,
   }
@@ -153,5 +153,59 @@ describe('Geolocation component with location during cooldown', () => {
     expect(fetchGeolocationSpy).toHaveBeenCalledTimes(0)
     await expect(getCoordinates).toHaveBeenCalledTimes(0)
     expect(getZipFromLatLonSpy).toHaveBeenCalledTimes(0)
+  })
+})
+
+describe('Geolocation component with location following cooldown', () => {
+  let testRenderer: any
+  let testInstance: any
+  const dispatch: any = jest.fn()
+  const geolocation: IGeolocation = {
+    lat: 30.2642,
+    lon: -97.7617,
+    timestamp: Date.now() - (1000 * GOOGLE_MAPS_COOL_OFF_SECONDS * 2),
+    zip: 78704,
+    isGeolocating: false,
+  }
+  const fetchGeolocationSpy = jest.spyOn(thunks, 'fetchGeolocation')
+  const getCoordinates = jest.spyOn(getCoordinatesModule, 'getCoordinates')
+  const getZipFromLatLonSpy = jest.spyOn(thunks, 'getZipFromLatLon')
+
+  beforeEach(() => {
+    testRenderer = TestRenderer.create(
+      <GeolocationButton
+        dispatch={dispatch}
+        geolocation={geolocation}
+      ></GeolocationButton>
+    )
+    testInstance = testRenderer.root;
+  })
+
+  it('properly renders geolocation button', () => {
+    expect.assertions(1)
+    expect(() => testInstance.findByType(GeolocationButton)).not.toThrow(Error)
+  })
+
+  it('renders proper classes', () => {
+    expect.assertions(1)
+    const geolocationTrigger = testInstance.findByProps({'data-id': 'geolocation-trigger'})
+    expect(geolocationTrigger.props.className).toBe('geolocation-trigger inset flash');
+  })
+
+  it('properly renders current zip', () => {
+    expect.assertions(2)
+    expect(() => testInstance.findByProps({'data-id': 'geolocation-zip'})).not.toThrow(Error)
+    const currentZip = testInstance.findByProps({'data-id': 'geolocation-zip'})
+    expect(currentZip.props.children).toBe(78704)
+  })
+
+  it('geolocates on click', async () => {
+    expect.assertions(4)
+    const geolocationTrigger = testInstance.findByProps({'data-id': 'geolocation-trigger'})
+    geolocationTrigger.props.onClick()
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(fetchGeolocationSpy).toHaveBeenCalledTimes(1)
+    await expect(getCoordinates).toHaveBeenCalledTimes(1)
+    expect(getZipFromLatLonSpy).toHaveBeenCalledTimes(1)
   })
 })
