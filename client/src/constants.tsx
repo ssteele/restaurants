734187fb8654675158761feda6036abfd3e65217
Steve Steele <steven.harville.steele@gmail.com>
2025-03-15 13:57:41 -0500

    Update frontend default location params

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -8,7 +8,9 @@ if ('development' === process.env.NODE_ENV) {
 }
 export const API_BASE_URL: string = apiBaseUrls[apiEnv]
 export const DEFAULT_CITY: string = 'austin'
-export const DEFAULT_ZIP: number = 78753
+export const DEFAULT_ZIP: number = 78751
+export const DEFAULT_LAT: number = 30.309239
+export const DEFAULT_LON: number = -97.719562
 export const DEFAULT_MAX_NEARBY_ZIP_MI_DISTANCE: number = 5
 // @todo: get lat/lon from phone gps and remove google everything
 export const GOOGLE_MAPS_COOL_OFF_SECONDS: number = 300
