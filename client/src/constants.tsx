73ee244bf4a932e53f0d768ceb16e559a459a173
Steve Steele <steven.harville.steele@gmail.com>
2025-03-22 14:48:26 -0500

    Remove deprecated constants

--- client/src/constants.tsx
+++ client/src/constants.tsx
@@ -8,14 +8,9 @@ if ('development' === process.env.NODE_ENV) {
 }
 export const API_BASE_URL: string = apiBaseUrls[apiEnv]
 export const DEFAULT_CITY: string = 'austin'
-export const DEFAULT_ZIP: number = 78751
 export const DEFAULT_LAT: number = 30.309239
 export const DEFAULT_LON: number = -97.719562
 export const DEFAULT_MAX_NEARBY_ZIP_MI_DISTANCE: number = 5
-// @todo: get lat/lon from phone gps and remove google everything
-export const GOOGLE_MAPS_API_ENDPOINT: string = 'https://maps.googleapis.com/maps/api/geocode/json'
-export const GOOGLE_MAPS_API_KEY: string | undefined = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-export const IS_GOOGLE_MAPS_ENABLED: boolean = 'true' === process.env.REACT_APP_IS_GOOGLE_MAPS_ENABLED
 export const LOCATION_REQUEST_COOL_OFF_SECONDS: number = 10
 export const MAX_NEARBY_ZIP_MI_DISTANCE_OPTIONS: { id: number, name: string }[] = [
   {id: 0, name: 'In current zip'},
